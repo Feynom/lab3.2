39c4a6bf84f040da7a7894b6037d39cd4b1963b0
Feynom <chreptak12@gmail.com>
2020-10-10 11:44:17 +0300

    lab3.2

---
+++ lab 3.2/lab 3.2.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double x, a, b, c, f;
+
+    cout << "Введіть значення 'x':";
+    cin >> x;
+    cout << endl;
+    cout << "Введіть значення 'a':";
+    cin >> a;
+    cout << endl;
+    cout << "Введіть значення 'b':";
+    cin >> b;
+    cout << endl;
+    cout << "Введіть значення 'c':";
+    cin >> c;
+    cout << endl;
+
+    if (x < 5 && c != 0)
+        f = -a * x * x - b;
+    if (x > 5 && c == 0)
+        f = (x - a) / x;
+    if (!(x < 5 && c != 0) && !(x > 5 && c == 0))
+        f = -x / c;
+    
+    cout << "1)Відповідь:" << f << endl;
+
+    if (x < 5 && c != 0)
+        f = -a * x * x - b;
+    else
+        if (x > 5 && c == 0)
+            f = (x - a) / x;
+        else
+            f = -x / c;
+
+    cout << "2)Відповідь:" << f << endl;
+
+    cin.get();
+    return 0;
+}
